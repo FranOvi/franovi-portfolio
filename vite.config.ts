612201bb04744504e310b3f7f8fe81d8d679aacd
FranOvi <francisco.oviedojr@yahoo.es>
2023-03-21 10:35:24 -0600

    Set router and vite config base route

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/franovi-portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/franovi-portfolio/' : '/',
   plugins: [
     vue({
       template: { transformAssetUrls }
